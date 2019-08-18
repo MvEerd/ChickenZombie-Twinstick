9f3f0d8ade338b509d5aad29ab54a4a07333be1a
MvEerd <michael@cache.works>
2019-08-18 01:52:23 -0700

    When shooting projectiles, face the shooting direction

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -16,20 +16,27 @@ public class PlayerControl : MonoBehaviour
     {
     }
 
-    public float minmax(float x, float min, float max, float new_min, float new_max)
+    public float MinMax(float x, float min, float max, float new_min, float new_max)
     {
-
         x = (x - min) * (new_max - new_min) / (max - min) + new_min;
         return x;
-
-
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+
+        float mouseAxisX = MinMax(Input.mousePosition.x, 0, Screen.width, -1, 1);
+        float mouseAxisY = MinMax(Input.mousePosition.y, 0, Screen.height, -1, 1);
+
+        float aimX = Input.GetMouseButton(0) ? mouseAxisX : Input.GetAxis("Mouse X"); 
+        float aimY = Input.GetMouseButton(0) ? mouseAxisY : Input.GetAxis("Mouse Y");
+        
+
+        timeSinceLastBullet += Time.deltaTime;
+
 
         if (moveX != 0 || moveY != 0)
         {
@@ -38,31 +45,26 @@ public class PlayerControl : MonoBehaviour
             tempVect = tempVect.normalized * speed * Time.deltaTime;
             rb.MovePosition(transform.position + tempVect);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3((float)moveX * speed, 0, (float)moveY * speed)), 1000 * Time.deltaTime);
+            if (aimX == 0 && aimY == 0)
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3((float)moveX * speed, 0, (float)moveY * speed)), 1000 * Time.deltaTime);
         } else
         {
             setAnimationState("");
         }
 
-        float mouseAxisX = minmax(Input.mousePosition.x, 0, Screen.width, -1, 1);
-        float mouseAxisY = minmax(Input.mousePosition.y, 0, Screen.height, -1, 1);
 
-        float mouseX = Input.GetMouseButton(0) ? mouseAxisX : Input.GetAxis("Mouse X"); 
-        float mouseY = Input.GetMouseButton(0) ? mouseAxisY : Input.GetAxis("Mouse Y");
-        
-
-        timeSinceLastBullet += Time.deltaTime;
-
-        if (mouseX != 0 || mouseY != 0)
+        if (aimX != 0 || aimY != 0)
         {
             float firerate = 0.09f; //0.015 ~ 0.5f
             float fireforce = 70f;
             float firettl = 2;
 
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3((float)aimX * speed, 0, (float)aimY * speed));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, 1000 * Time.deltaTime);
+
             if (timeSinceLastBullet < firerate) return;
             timeSinceLastBullet = 0;
 
-            Quaternion lookRotation = Quaternion.LookRotation(new Vector3((float)mouseX * speed, 0, (float)mouseY * speed));
             Vector3 bulletBuffer = lookRotation * new Vector3(0, 0, 0.5f);
 
             GameObject bullet = Instantiate(projectile, transform.position + bulletBuffer + new Vector3(0, 0.2f,0), lookRotation) as GameObject;
