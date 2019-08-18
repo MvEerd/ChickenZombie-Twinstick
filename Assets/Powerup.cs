8d1c9a0389538d4b6ecba2a61d01aac29c98b292
MvEerd <michael@cache.works>
2019-08-18 08:51:32 -0700

    Add powerup blocks to change weapons

---
+++ Assets/Powerup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public Gun powerupGun;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<PlayerControl>().currentGun = powerupGun;
+    }
+}
