4926e29ceb786eb2590ab246202381d8d802bf4f
MvEerd <michael@cache.works>
2019-08-23 14:25:01 -0700

    Set _health and healthBar maxValue to configured enemy health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,8 +20,8 @@ public class Enemy : MonoBehaviour
     public List<Lootobject> lootTable = new List<Lootobject>();
     public int dropRate;
     private bool alive = true;
-    private float _health = 100;
     public float maxHealth = 100;
+    private float _health = 100;
     public float attackDelay = 1;
     float _lastAttack = 0;
     public GameObject healthbarCanvas;
@@ -31,6 +31,8 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _health = maxHealth;
+        healthBar.maxValue = maxHealth;
         target = GameObject.Find("Player");
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = true;
